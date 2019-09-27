3c42927d0fb077baee266d0704fb591614d4c569
Vinicius Oliveira <vinicius.oliveira@anexsdev.com.br>
2019-09-27 14:22:12 -0300

    .

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -6,6 +6,8 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 
 const image1 = require('../../resources/images/produto-01.jpeg');
+const plus = require('../../resources/icons/baseline-add-24px.svg');
+const less = require('../../resources/icons/baseline-remove-24px.svg');
 
 const Product = () => (
     <Card className="card-container">
@@ -17,37 +19,57 @@ const Product = () => (
                 <p className="productValueSecond">Em até 12x de R$ 124,92</p>
                 <p className="productValueSecond">R$ 1.349 à vista (10% de desconto)</p>
             </div>
-            <span className="add-product">
-                <div className="product-add">
-                    <span className="product-plus">+</span>
-                    <input className="product-quantity" />
-                    <span className="product-less">-</span>
+            <div className="add-product">
+                <div className="topControl">
+                    <span className="product-less">
+                        {/* <img src={less} /> */}
+                        -
+                    </span>
+                    <input value="0" />
+                    <span>
+                        {/* <img src={plus} /> */}
+                        +
+                    </span>
                 </div>
-                <div className="product-button">
-                    <Button variant="contained" color="primary">
-                        Primary
-                    </Button> 
+                <div className="bottomControl">
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button> 
                  </div>
-            </span>
+            </div>
         </CardContent>
     </Card>
-    // <div className="products">
-    //     <img src={image1}/>
-    //     <div className="info-products">
-    // <p className="productName">AirPods Apple Fones de Ouvido</p>
-    // <p className="productValue">R$ 1.499,00</p>
-    // <p className="productValueSecond">Em até 12x de R$ 124,92</p>
-    // <p className="productValueSecond">R$ 1.349 à vista (10% de desconto)</p>
-    //     </div>
-    // <div className="product-add">
-    //     <span className="product-plus">+</span>
-    //     <input className="product-quantity"/>
-    //     <span className="product-less">-</span>
-    // </div>
-    // <div className="product-button">
-    //     <button>Adicionar</button>
-    // </div>
-    // </div>
 );
 
 export default Product;
+
+
+
+
+// <Card className="card-container">
+// <img src={image1} />
+// <CardContent className="cardContent">
+//     <div className="productInfo">
+//         <p className="productName">AirPods Apple Fones de Ouvido</p>
+//         <p className="productValue">R$ 1.499,00</p>
+//         <p className="productValueSecond">Em até 12x de R$ 124,92</p>
+//         <p className="productValueSecond">R$ 1.349 à vista (10% de desconto)</p>
+//     </div>
+//     <div className="add-product">
+//         <div className="product-add">
+//             <span className="product-less">
+//                 <img src={less} />
+//             </span>
+//             <input value="0" className="product-quantity" />
+//             <span className="product-plus">
+//                 <img src={plus} />
+//             </span>
+//         </div>
+//         <div className="product-button">
+//         <Button variant="contained" color="primary">
+//             Primary
+//         </Button> 
+//          </div>
+//     </div>
+// </CardContent>
+// </Card>
