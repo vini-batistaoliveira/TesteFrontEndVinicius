ccd4185bc6ec168daa1abd7adc124e0eaaed6bfc
Vinicius Oliveira <vinicius.batistaoli@gmail.com>
2019-10-01 20:52:41 -0300

    Ajustes finais container, cores preço, tamanho botão

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -12,7 +12,7 @@ import Form from "../Form/index";
 class Main extends Component {
   render() {
     return (
-      <div>
+      <div classname="fullAppContainer">
         <Header name={"Produtos"} />
         <div className="product-list">
           <Grid
